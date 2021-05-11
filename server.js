87cd8a9fe706d403c99d789d78ec01b9ee17d79e
Shay Wagner <srwagner916@gmail.com>
2021-05-11 17:31:15 -0500

    created post route

--- server.js
+++ server.js
@@ -5,11 +5,21 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // get data from db.
 app.get('/api/notes', (req, res) => {
   res.json(data);
+});
+
+// Post route for new note
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  req.body.id = data.length.toString();
+  console.log(newNote);
+  res.json(newNote);
 });
 
 // serve notes.html
